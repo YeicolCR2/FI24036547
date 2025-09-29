e3569a2618659aea92d4845c632c788b10a5aef2
YeicolCR02 <yeicol2001s@gmail.com>
2025-09-28 18:49:13 -0600

    agrego practica PP1

--- PP1/inverso.java/src/inverso/java/InversoJava.java
+++ PP1/inverso.java/src/inverso/java/InversoJava.java
@@ -4,17 +4,32 @@
  */
 package inverso.java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Games
  */
 public class InversoJava {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       int invertir(int num, int invertido) {
+        if (num>0){
+        return invertir(num / 10, invertido * 10 + num % 10);
+    }else{
+            return invertido;
+        }
+   }
     
-}
+    
+    public static void main(String[] args) {
+       InversoJava numInv = new InversoJava();
+       Scanner sc = new Scanner(System.in);
+       int num = 0, invertido = 0;
+       while(num <= 0){
+       System.out.println("ingrese un numero mayor que 0");
+       num  = sc.nextInt();
+       }
+        System.out.println("Numero original: " + num + " y el numero invertido ->  " + numInv.invertir(num, invertido) );
+    }
+          
+} 
